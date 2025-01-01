adf8c8c2d36901c19711cf0b06093146381ba443
Yiyan Peng <harrypengyiyan@163.com>
2025-01-01 21:30:12 +0800

    update code

--- pytorch_ypengbb/scripts/machine_temp_script.sh
+++ pytorch_ypengbb/scripts/machine_temp_script.sh
@@ -1,3 +1,5 @@
 # need to add JSON file path in the argparser (specified in utils.py -> get_args())
 
-python pytorch_anomaly_detection_main.py -c ./pytorch_NAB_config.json
+python preprocess_NAB_data.py -c pytorch_NAB_config.json \
+&& \
+python pytorch_anomaly_detection_main.py -c pytorch_NAB_config.json
