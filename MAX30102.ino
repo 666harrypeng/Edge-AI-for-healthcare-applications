5f19d42ac4055f34f12b4c1cc395378e4a548b58
chris kuo <luckychris60404@gmail.com>
2025-01-21 17:34:20 +0800

    Update MAX30102.ino

--- MAX30102.ino
+++ MAX30102.ino
@@ -1,194 +1,118 @@
-#include <Wire.h>
-#include "MAX30105.h"
-#include "spo2_algorithm.h"
-#include "heartRate.h"
+#include "MAX30105.h"           //MAX3010x library
+#include "heartRate.h"          //Heart rate calculating algorithm
 
 MAX30105 particleSensor;
 
-#define MAX_BRIGHTNESS 255
-
-uint32_t irBuffer[100]; //infrared LED sensor data
-uint32_t redBuffer[100];  //red LED sensor data
- 
-#define REPORTING_PERIOD_MS 1000 // frequency of updates sent to blynk app in ms
- 
-char auth[] = "";             // You should get Auth Token in the Blynk App.
-char ssid[] = "";                              // Your WiFi credentials.
-char pass[] = "";
- 
-uint32_t tsLastReport = 0;  //stores the time the last update was sent to the blynk app
-
-int32_t bufferLength; //data length
-int32_t spo2; //SPO2 value
-int8_t validSPO2; //indicator to show if the SPO2 calculation is valid
-int32_t heartRate; //heart rate value calcualated as per Maxim's algorithm
-int8_t validHeartRate; //indicator to show if the heart rate calculation is valid
-
-byte pulseLED = 2; //onboard led on esp32 nodemcu
-byte readLED = 19; //Blinks with each data read 
-
+//計算心跳用變數
+const byte RATE_SIZE = 4; //多少平均數量
+byte rates[RATE_SIZE]; //心跳陣列
+byte rateSpot = 0;
 long lastBeat = 0; //Time at which the last beat occurred
+float beatsPerMinute;
+int beatAvg;
 
-float beatsPerMinute; //stores the BPM as per custom algorithm
-int beatAvg = 0, sp02Avg = 0; //stores the average BPM and SPO2 
-float ledBlinkFreq; //stores the frequency to blink the pulseLED
+//計算血氧用變數
+double avered = 0;
+double aveir = 0;
+double sumirrms = 0;
+double sumredrms = 0;
 
- 
-void setup()
-{ 
-  ledcSetup(0, 0, 8); // PWM Channel = 0, Initial PWM Frequency = 0Hz, Resolution = 8 bits
-  ledcAttachPin(pulseLED, 0); //attach pulseLED pin to PWM Channel 0
-  ledcWrite(0, 255); //set PWM Channel Duty Cycle to 255
-  
+double SpO2 = 0;
+double ESpO2 = 60.0;//初始值
+double FSpO2 = 0.7; //filter factor for estimated SpO2
+double frate = 0.95; //low pass filter for IR/red LED value to eliminate AC component
+int i = 0;
+int Num = 30;//取樣30次才計算1次
+#define FINGER_ON 7000    //紅外線最小量（判斷手指有沒有上）
+#define MINIMUM_SPO2 60.0 //血氧最小量
+
+void setup() {
   Serial.begin(115200);
-  
-  Serial.print("Initializing Pulse Oximeter..");
-  
-    // Initialize sensor
+  Serial.println("System Start");
+  delay(3000);
+  //檢查
   if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
   {
-    Serial.println(F("MAX30105 was not found. Please check wiring/power."));
+    Serial.println("找不到MAX30102");
     while (1);
   }
-
-
-  /*The following parameters should be tuned to get the best readings for IR and RED LED. 
-   *The perfect values varies depending on your power consumption required, accuracy, ambient light, sensor mounting, etc. 
-   *Refer Maxim App Notes to understand how to change these values
-   *I got the best readings with these values for my setup. Change after going through the app notes.
-   */
-  byte ledBrightness = 50; //Options: 0=Off to 255=50mA
-  byte sampleAverage = 1; //Options: 1, 2, 4, 8, 16, 32
-  byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
-  byte sampleRate = 100; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
-  int pulseWidth = 69; //Options: 69, 118, 215, 411
-  int adcRange = 4096; //Options: 2048, 4096, 8192, 16384
-  
+  //以下選項可自行調整
+  byte ledBrightness = 0x7F; //亮度建議=127, Options: 0=Off to 255=50mA
+  byte sampleAverage = 4; //Options: 1, 2, 4, 8, 16, 32
+  byte ledMode = 2; //Options: 1 = Red only(心跳), 2 = Red + IR(血氧)
+  int sampleRate = 800; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
+  int pulseWidth = 215; //Options: 69, 118, 215, 411
+  int adcRange = 16384; //Options: 2048, 4096, 8192, 16384
+  // Set up the wanted parameters
   particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
+  particleSensor.enableDIETEMPRDY();
+
+  particleSensor.setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
+  particleSensor.setPulseAmplitudeGreen(0); //Turn off Green LED
 }
- 
-void loop()
-{
-  bufferLength = 100; //buffer length of 100 stores 4 seconds of samples running at 25sps
-  
-  //read the first 100 samples, and determine the signal range
-  for (byte i = 0 ; i < bufferLength ; i++)
-  {
-    while (particleSensor.available() == false) //do we have new data?
-      particleSensor.check(); //Check the sensor for new data
-  
-    redBuffer[i] = particleSensor.getIR();
-    irBuffer[i] = particleSensor.getRed();
-    particleSensor.nextSample(); //We're finished with this sample so move to next sample
-  
-    Serial.print(F("red: "));
-    Serial.print(redBuffer[i], DEC);
-    Serial.print(F("\t ir: "));
-    Serial.println(irBuffer[i], DEC);
-  }
-  
-  //calculate heart rate and SpO2 after first 100 samples (first 4 seconds of samples)
-  maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
-  
-  //Continuously taking samples from MAX30102.  Heart rate and SpO2 are calculated every 1 second
-  while (1)
-  {
-    //dumping the first 25 sets of samples in the memory and shift the last 75 sets of samples to the top
-    for (byte i = 25; i < 100; i++)
-    {
-      redBuffer[i - 25] = redBuffer[i];
-      irBuffer[i - 25] = irBuffer[i];
+
+void loop() {
+  long irValue = particleSensor.getIR();    //Reading the IR value it will permit us to know if there's a finger on the sensor or not
+  //是否有放手指
+  if (irValue > FINGER_ON ) {
+    //檢查是否有心跳，測量心跳
+    if (checkForBeat(irValue) == true) {
+      //Serial.print("Bpm="); Serial.println(beatAvg);//將心跳顯示到序列
+      long delta = millis() - lastBeat;//計算心跳差
+      lastBeat = millis();
+      beatsPerMinute = 60 / (delta / 1000.0);//計算平均心跳
+      if (beatsPerMinute < 255 && beatsPerMinute > 20) {
+        //心跳必須再20-255之間
+        rates[rateSpot++] = (byte)beatsPerMinute; //儲存心跳數值陣列
+        rateSpot %= RATE_SIZE;
+        beatAvg = 0;//計算平均值
+        for (byte x = 0 ; x < RATE_SIZE ; x++) beatAvg += rates[x];
+        beatAvg /= RATE_SIZE;
+      }
     }
-  
-    //take 25 sets of samples before calculating the heart rate.
-    for (byte i = 75; i < 100; i++)
-    {
-      while (particleSensor.available() == false) //do we have new data?
-        particleSensor.check(); //Check the sensor for new data
-    
-      digitalWrite(readLED, !digitalRead(readLED)); //Blink onboard LED with every data read
-    
-      redBuffer[i] = particleSensor.getRed();
-      irBuffer[i] = particleSensor.getIR();
+
+    //測量血氧
+    uint32_t ir, red ;
+    double fred, fir;
+    particleSensor.check(); //Check the sensor, read up to 3 samples
+    if (particleSensor.available()) {
+      i++;
+      ir = particleSensor.getFIFOIR(); //讀取紅外線
+      red = particleSensor.getFIFORed(); //讀取紅光
+      //Serial.println("red=" + String(red) + ",IR=" + String(ir) + ",i=" + String(i));
+      fir = (double)ir;//轉double
+      fred = (double)red;//轉double
+      aveir = aveir * frate + (double)ir * (1.0 - frate); //average IR level by low pass filter
+      avered = avered * frate + (double)red * (1.0 - frate);//average red level by low pass filter
+      sumirrms += (fir - aveir) * (fir - aveir);//square sum of alternate component of IR level
+      sumredrms += (fred - avered) * (fred - avered); //square sum of alternate component of red level
+
+      if ((i % Num) == 0) {
+        double R = (sqrt(sumirrms) / aveir) / (sqrt(sumredrms) / avered);
+        SpO2 = -23.3 * (R - 0.4) + 120;
+        ESpO2 = FSpO2 * ESpO2 + (1.0 - FSpO2) * SpO2;//low pass filter
+        if (ESpO2 <= MINIMUM_SPO2) ESpO2 = MINIMUM_SPO2; //indicator for finger detached
+        if (ESpO2 > 100) ESpO2 = 99.9;
+        //Serial.print(",SPO2="); Serial.println(ESpO2);
+        sumredrms = 0.0; sumirrms = 0.0; SpO2 = 0;
+        i = 0;
+      }
       particleSensor.nextSample(); //We're finished with this sample so move to next sample
-
-      //send samples and calculation result to terminal program through UART
-      //Uncomment these statements to view the raw data during calibration of sensor.
-      //When uncommented, beatsPerMinute will be slightly off.
-      /*Serial.print(F("red: "));
-      Serial.print(redBuffer[i], DEC);
-      Serial.print(F("\t ir: "));
-      Serial.print(irBuffer[i], DEC);
-      Serial.print(F("\t HR="));
-      Serial.print(heartRate, DEC);
-      Serial.print(F("\t"));
-      Serial.print(beatAvg, DEC);
-      
-      Serial.print(F("\t HRvalid="));
-      Serial.print(validHeartRate, DEC);
-      
-      Serial.print(F("\t SPO2="));
-      Serial.print(spo2, DEC);
-      
-      Serial.print(F("\t SPO2Valid="));
-      Serial.println(validSPO2, DEC);*/
-
-      long irValue = irBuffer[i];
-
-      //Calculate BPM independent of Maxim Algorithm. 
-      if (checkForBeat(irValue) == true)
-      {
-        //We sensed a beat!
-        long delta = millis() - lastBeat;
-        lastBeat = millis();
-      
-        beatsPerMinute = 60 / (delta / 1000.0);
-        beatAvg = (beatAvg+beatsPerMinute)/2;
-
-        if(beatAvg != 0)
-          ledBlinkFreq = (float)(60.0/beatAvg);
-        else
-          ledBlinkFreq = 0;
-        ledcWriteTone(0, ledBlinkFreq);
-      }
-      if(millis() - lastBeat > 10000)
-      {
-        beatsPerMinute = 0;
-        beatAvg = (beatAvg+beatsPerMinute)/2;
-        
-        if(beatAvg != 0)
-          ledBlinkFreq = (float)(60.0/beatAvg);
-        else
-          ledBlinkFreq = 0;
-        ledcWriteTone(0, ledBlinkFreq);
-      }
     }
-  
-    //After gathering 25 new samples recalculate HR and SP02
-    maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
     
-    Serial.print(beatAvg, DEC);
-    
-    Serial.print(F("\t HRvalid="));
-    Serial.print(validHeartRate, DEC);
-    
-    Serial.print(F("\t SPO2="));
-    Serial.print( sp02Avg , DEC);
-    
-    Serial.print(F("\t SPO2Valid="));
-    Serial.println(validSPO2, DEC);
-
-    //Calculates average SPO2 to display smooth transitions on Blynk App
-    if(validSPO2 == 1 && spo2 < 100 && spo2 > 0)
-    {
-      sp02Avg = (sp02Avg+spo2)/2;
-    }
-    else
-    {
-      spo2 = 0;
-      sp02Avg = (sp02Avg+spo2)/2;;
-    }
-
-    
+    //將數據顯示到序列
+    Serial.print("Bpm:" + String(beatAvg));
+    //顯示血氧數值，避免誤測，規定心跳超過30才能顯示血氧
+    if (beatAvg > 30)  Serial.println(",SPO2:" + String(ESpO2));
+    else Serial.println(",SPO2:" + String(ESpO2));
+  }
+  //沒偵測到手指，清除所有數據及螢幕內容顯示"Finger Please"
+  else {
+    //清除心跳數據
+    for (byte rx = 0 ; rx < RATE_SIZE ; rx++) rates[rx] = 0;
+    beatAvg = 0; rateSpot = 0; lastBeat = 0;
+    //清除血氧數據
+    avered = 0; aveir = 0; sumirrms = 0; sumredrms = 0;
+    SpO2 = 0; ESpO2 = 90.0;
   }
 }
